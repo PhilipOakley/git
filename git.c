535432af5522b07665af690f0ae9ccc47b736d44
Philip Oakley <philipoakley@iee.org>
2013-03-04 22:49:57 +0000

    Show help: -a and -g option, and 'git help <concept>' usage.
    Prepare for the addition of the -g --guides option to git help
    and show that help is available for both concept guides, and commands.
    
    Signed-off-by: Philip Oakley <philipoakley@iee.org>

--- git.c
+++ git.c
@@ -13,7 +13,9 @@ const char git_usage_string[] =
 	"           <command> [<args>]";
 
 const char git_more_info_string[] =
-	N_("See 'git help <command>' for more information on a specific command.");
+	N_("'git help -a' and 'git help -g' lists available subcommands\n"
+	   "and some concept guides. See 'git help <command>' or 'git help\n"
+	   "<concept>' to read about a specific subcommand or concept.");
 
 static struct startup_info git_startup_info;
 static int use_pager = -1;
