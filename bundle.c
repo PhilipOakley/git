2a9651099e9cf0c1e915bf74a05182fd9b13f1e5
Philip Oakley <philipoakley@iee.org>
2016-03-26 23:55:49 +0000

    'nother try (failed - no change)

--- bundle.c
+++ bundle.c
@@ -394,8 +394,11 @@ static int write_bundle_refs(int bundle_fd, struct rev_info *revs)
 		write_or_die(bundle_fd, display_ref, strlen(display_ref));
 		/* if the display_ref is the HEAD symref then post fix its true identity */
 		if (!strcmp(display_ref, "HEAD")) write_or_die(bundle_fd, "\0HEAD", 5);
+		/*
 		if (!strcmp(display_ref, "HEAD")) write_or_die(bundle_fd, head_ref, strlen(head_ref));
 		if (!strcmp(display_ref, head_ref)) write_or_die(bundle_fd, "\0HEAD", 5);
+		*/
+		write_or_die(bundle_fd, head_ref, strlen(head_ref));
 		write_or_die(bundle_fd, "\n", 1);
  skip_write_ref:
 		free(ref);
