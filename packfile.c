884df0b3c4a63f3bd46012503c2ab846e752336d
Thomas Braun <thomas.braun@virtuell-zuhause.de>
2019-01-31 10:24:06 +0100

    WIP: long is not important anymore
    Signed-off-by: Thomas Braun <thomas.braun@virtuell-zuhause.de>

--- packfile.c
+++ packfile.c
@@ -1056,7 +1056,7 @@ size_t unpack_object_header_buffer(const unsigned char *buf,
 	size = c & 15;
 	shift = 4;
 	while (c & 0x80) {
-		if (len <= used || bitsizeof(long) <= shift) {
+		if (len <= used) {
 			error("bad object header");
 			size = used = 0;
 			break;
