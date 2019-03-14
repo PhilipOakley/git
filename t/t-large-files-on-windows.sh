467cc5d8740b7dfcee60fcb2701b7f68cd60635f
Philip Oakley <philipoakley@iee.org>
2019-03-14 11:16:24 +0000

    t-large-files-on-windows.sh: extra fsck and not EXPENSIVE
    Couldn't work out the command line argument to cancel the EXPENSIVE.
    
    Add the extra fsck to get diagnostics after the add.
    
    Signed-off-by: Philip Oakley <philipoakley@iee.org>

--- t/t-large-files-on-windows.sh
+++ t/t-large-files-on-windows.sh
@@ -3,12 +3,13 @@
 test_description='test large file handling on windows'
 . ./test-lib.sh
 
-test_expect_success EXPENSIVE,SIZE_T_IS_64BIT 'blah blubb' '
+test_expect_success SIZE_T_IS_64BIT 'blah blubb' '
 
 	dd if=/dev/zero of=file bs=1M count=4100 &&
 	git config core.compression 0 &&
 	git config core.looseCompression 0 &&
 	git add file &&
+	git fsck --verbose --strict --full &&
 	git commit -m msg file &&
 	git log --stat &&
 	git fsck --verbose --strict --full &&
