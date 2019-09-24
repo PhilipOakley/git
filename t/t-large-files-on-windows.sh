dda9de4fc8d5687bd48295003011f02756dc267b
Philip Oakley <philipoakley@iee.email>
2019-09-24 23:18:51 +0100

    fixup! Add test for large files on windows
    include ZLIB_IS_LLP64 check
    
    --unset pack.packsizelimit for Windows manual testing to ensure
    `git add file` produces a *loose* object.
    This is to avoid use of crc32 fn within the pack files.
    
    Signed-off-by: Philip Oakley <philipoakley@iee.email>

--- t/t-large-files-on-windows.sh
+++ t/t-large-files-on-windows.sh
@@ -3,22 +3,39 @@
 test_description='test large file handling on windows'
 . ./test-lib.sh
 
-test_expect_success SIZE_T_IS_64BIT 'require 64bit size_t' '
+test_expect_success SIZE_T_IS_64BIT,ZLIB_IS_LLP64 'setup >4GiB file' '
 
-	test-tool zlib-compile-flags >zlibFlags.txt &&
+	git init &&
 	dd if=/dev/zero of=file bs=1M count=4100 &&
 	git config core.compression 0 &&
 	git config core.looseCompression 0 &&
+	# git config --get-all pack.packsizelimit >pack.psl.txt &&
+	# git config --unset pack.packsizelimit 
+	echo "test" .tst.txt
+	# do not git config --unset core.bigFileThreshold we only want to avoid delta compression and keep zlib deflation at zero compression
+'
+
+test_expect_success SIZE_T_IS_64BIT,ZLIB_IS_LLP64 '>4GiB loose object' '
+
+	: just check zlib deflation and not the large pack crc32
 	git add file &&
-	git verify-pack -s .git/objects/pack/*.pack &&
-	git fsck --verbose --strict --full &&
+	git fsck --verbose --strict --full 
+'
+
+test_expect_success SIZE_T_IS_64BIT,ZLIB_IS_LLP64 '>4GiB pack file with commit' '
+
+	# check the packing and crc32 over a large >4Gb file
+	git repack -adf &&
+	# need to ensure we get a pack... 
 	git commit -m msg file &&
-	git log --stat &&
-	git gc &&
-	git fsck --verbose --strict --full &&
-	git index-pack -v -o test.idx .git/objects/pack/*.pack &&
-	git gc &&
-	git fsck
+	# test here make sure it is packed correctly
+	git verify-pack -s .git/objects/pack/*.pack &&
+	git fsck --verbose --strict --full
 '
 
 test_done
+# core.repositoryformatversion ?
+# core.compression
+# core.looseCompression and pack.compression
+# core.packedGitWindowSize
+# core.bigFileThreshold ***
